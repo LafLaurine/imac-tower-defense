5419935561cf14572bec796df242ae10157d2c30
Laurine <30570842+LafLaurine@users.noreply.github.com>
2019-04-01 21:29:43 +0200

    Add files via upload

---
+++ include/monster.h
@@ -0,0 +1,35 @@
+#ifndef MONSTER
+#define MONSTER
+
+#include "node.h"
+
+typedef struct Monster {
+
+	//position du monstre
+	float x;
+	float y;
+	int direction;
+    //son chemin = noeud
+	//Noeud précédent (carte)
+	Node* node_prev;
+	//Noeud suivant (carte)
+	Node* node_next;
+	//type de monstre
+	char* type;
+	//point de vie
+	int pv;
+	//qd il meurt
+	int money;
+}Monster;
+
+//liste monstre
+typedef struct List_Monster {
+
+	//Taille de la liste
+	size_t length;
+	Monster *m_first; //pointeur vers le premier element
+	Monster *m_last; //pointeur vers le dernier element
+
+}List_Monster;
+
+#endif
