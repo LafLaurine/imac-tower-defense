59ad2daff1af5f4d58ba702b9427382f79022e4c
LafLafontaine <stupid.lauw@hotmail.fr>
2019-05-14 08:42:02 +0200

    Argument executable

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ void init_window() {
 }
 
 
-int main (int argc, char** argv)
+int main (int argc, char* argv[])
 {
     if(-1 == SDL_Init(SDL_INIT_VIDEO)) 
     {
@@ -52,7 +52,6 @@ int main (int argc, char** argv)
             "Impossible d'initialiser la SDL. Fin du programme.\n");
         exit(EXIT_FAILURE);
     }
-  
     /* Ouverture d'une fenetre et creation d'un contexte OpenGL */
     init_window();
     reshape();
@@ -64,158 +63,161 @@ int main (int argc, char** argv)
     /* Initialisation du titre de la fenetre */
     SDL_WM_SetCaption(WINDOW_TITLE, NULL);
 
-    int play = 0;
-    int help = 0;
+    if (argc > 0) {
 
-    //Textures et surfaces associées
-    SDL_Surface* surface;
-    
-    //Map
-    GLuint texture_map;
-    SDL_Surface* s_map = NULL;
+        int play = 0;
+        int help = 0;
 
-    //Monster
-    GLuint texture_monster;
-    SDL_Surface* s_monster = NULL; 
-
-    //Help
-    GLuint help_txt;
-    SDL_Surface* help_surface = NULL;   
-
-    GLuint tower_texture;
-
-    //Init game
-    Game *game = new_game();
-
-    //Create NODE
-    List_Node* list_node = new_List_Node();
-    add_node(list_node, 10, 10);
-    Map* map = init_map("./data/map01.itd");
-
-    // Test check segment
-    //check_segment(300, 100, 450, 100, map);
-
-    // Create list monster
-    List_Monster* l_monster = new_monster_list();
-
-    //Create list tower
-    List_Tower* l_tower =  new_tower_list();
-
-    Monster* monster = NULL;
-    Tower* tower = NULL;
-
-    int loop = 1;
-
-    while(loop) 
-    {
-        /* Recuperation du temps au debut de la boucle */
-        Uint32 startTime = SDL_GetTicks();
+        //Textures et surfaces associées
+        SDL_Surface* surface;
         
-        /* Placer ici le code de dessin */
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_MODELVIEW);
+        //Map
+        GLuint texture_map;
+        SDL_Surface* s_map = NULL;
 
-         // Check map
-        s_map = load_sprite(map->img->path,&texture_map);
-        s_monster = load_sprite("./images/monster.jpg", &texture_monster);
-        help_surface = load_sprite("./images/aide.jpg", &help_txt);
-        display_map(&texture_map);
-        display_path(map);
+        //Monster
+        GLuint texture_monster;
+        SDL_Surface* s_monster = NULL; 
 
-        //Vague monstre
+        //Help
+        GLuint help_txt;
+        SDL_Surface* help_surface = NULL;   
 
+        GLuint tower_texture;
 
-        //Dessiner les tours
-        display_tower(&tower_texture, l_tower, l_monster, tower);
+        //Init game
+        Game *game = new_game();
 
-        //Dessiner les monstres
+        //Create NODE
+        List_Node* list_node = new_List_Node();
+        add_node(list_node, 10, 10);
+        Map* map = init_map(argv[1]);
 
-        if(help == 1){
-            display_help(&help_txt);
-        }
-    
-        
-        /* Echange du front et du back buffer : mise a jour de la fenetre */
-        SDL_GL_SwapBuffers();
-        
-        /* Boucle traitant les evenements */
-        SDL_Event e;
-        while(SDL_PollEvent(&e)) 
+        // Test check segment
+        //check_segment(300, 100, 450, 100, map);
+
+        // Create list monster
+        List_Monster* l_monster = new_monster_list();
+
+        //Create list tower
+        List_Tower* l_tower =  new_tower_list();
+
+        Monster* monster = NULL;
+        Tower* tower = NULL;
+
+        int loop = 1;
+
+        while(loop) 
         {
-            /* L'utilisateur ferme la fenetre : */
-            if(e.type == SDL_QUIT) 
-            {
-                loop = 0;
-                break;
+            /* Recuperation du temps au debut de la boucle */
+            Uint32 startTime = SDL_GetTicks();
+            
+            /* Placer ici le code de dessin */
+            glClear(GL_COLOR_BUFFER_BIT);
+            glMatrixMode(GL_MODELVIEW);
+
+             // Check map
+            s_map = load_sprite(map->img->path,&texture_map);
+            s_monster = load_sprite("./images/monster.jpg", &texture_monster);
+            help_surface = load_sprite("./images/aide.jpg", &help_txt);
+            display_map(&texture_map);
+            display_path(map);
+
+            //Vague monstre
+
+
+            //Dessiner les tours
+            display_tower(&tower_texture, l_tower, l_monster, tower);
+
+            //Dessiner les monstres
+
+            if(help == 1){
+                display_help(&help_txt);
             }
-    
-            /* Quelques exemples de traitement d'evenements : */
-            switch(e.type) 
-            {
-                /* Redimensionnement fenetre */
-                case SDL_VIDEORESIZE:
-                    WINDOW_WIDTH = e.resize.w;
-                    WINDOW_HEIGHT = e.resize.h;
-                    init_window();
-                    break;
-
-                /* Clic souris */
-                case SDL_MOUSEBUTTONUP:
-                    printf("clic en (%d, %d)\n", e.button.x, e.button.y);
-                    break;
-                
-                /* Touche clavier */
-                case SDL_KEYDOWN:
-                switch(e.key.keysym.sym){
-                        case 'p' :
-                            if(play == 0)
-                                play = 1;
-                            else
-                                play = 0;
-                            break;
-
-                        case 'h' :
-                        if(help == 0)
-                        {
-                            help = 1;
-                        }
-                        else {
-                            help = 0;
-                        }
-                            break;
-
-                            case 'q' : 
-                            case SDLK_ESCAPE : 
-                            loop = 0;
-                            break;
-
-                            default : break;
-                    }
-                    break;
-              
-                default:
-                    break;
-            }
-        }
-
-        /* Calcul du temps ecoule */
-        Uint32 elapsedTime = SDL_GetTicks() - startTime;
-        /* Si trop peu de temps s'est ecoule, on met en pause le programme */
-        if(play == 0) {
-            if(elapsedTime < FRAMERATE_MILLISECONDS) {
-                SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
-            }
-        }
-        else if(play == 2) {
-            if(elapsedTime < FRAMERATE_MILLISECONDS_FAST) {
-                    SDL_Delay(FRAMERATE_MILLISECONDS_FAST - elapsedTime);
-            }
-        }
         
-    }
+            
+            /* Echange du front et du back buffer : mise a jour de la fenetre */
+            SDL_GL_SwapBuffers();
+            
+            /* Boucle traitant les evenements */
+            SDL_Event e;
+            while(SDL_PollEvent(&e)) 
+            {
+                /* L'utilisateur ferme la fenetre : */
+                if(e.type == SDL_QUIT) 
+                {
+                    loop = 0;
+                    break;
+                }
+        
+                /* Quelques exemples de traitement d'evenements : */
+                switch(e.type) 
+                {
+                    /* Redimensionnement fenetre */
+                    case SDL_VIDEORESIZE:
+                        WINDOW_WIDTH = e.resize.w;
+                        WINDOW_HEIGHT = e.resize.h;
+                        init_window();
+                        break;
 
-    glDeleteTextures(1,&texture_map);
-    game_end(game);
+                    /* Clic souris */
+                    case SDL_MOUSEBUTTONUP:
+                        printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+                        break;
+                    
+                    /* Touche clavier */
+                    case SDL_KEYDOWN:
+                    switch(e.key.keysym.sym){
+                            case 'p' :
+                                if(play == 0)
+                                    play = 1;
+                                else
+                                    play = 0;
+                                break;
+
+                            case 'h' :
+                            if(help == 0)
+                            {
+                                help = 1;
+                            }
+                            else {
+                                help = 0;
+                            }
+                                break;
+
+                                case 'q' : 
+                                case SDLK_ESCAPE : 
+                                loop = 0;
+                                break;
+
+                                default : break;
+                        }
+                        break;
+                  
+                    default:
+                        break;
+                }
+            }
+
+            /* Calcul du temps ecoule */
+            Uint32 elapsedTime = SDL_GetTicks() - startTime;
+            /* Si trop peu de temps s'est ecoule, on met en pause le programme */
+            if(play == 0) {
+                if(elapsedTime < FRAMERATE_MILLISECONDS) {
+                    SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
+                }
+            }
+            else if(play == 2) {
+                if(elapsedTime < FRAMERATE_MILLISECONDS_FAST) {
+                        SDL_Delay(FRAMERATE_MILLISECONDS_FAST - elapsedTime);
+                }
+            }
+            
+        }
+
+        glDeleteTextures(1,&texture_map);
+        game_end(game);
+    }
     /* Liberation des ressources associees a la SDL */ 
     SDL_Quit();
     return EXIT_SUCCESS;
