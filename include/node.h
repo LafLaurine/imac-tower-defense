19923b7bb2faa2033696bc9edad3e4e28aad07ad
Claclicla <clara.daigmorte@gmail.com>
2019-06-04 23:34:15 +0200

    node propre

--- include/node.h
+++ include/node.h
@@ -1,47 +1,47 @@
 #ifndef NODE_H
 #define NODE_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "colors.h"
 #define NB_MIN_NOEUDS 2
 
-typedef enum{In, Out, Twist, Intersect} Node_Type;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "colors.h"
+
+
+typedef enum {
+	In, Out, Twist, Intersect
+} Node_Type;
+
 
 typedef struct Node {
-	//x node coordonate
-	float x;
-	//y node coordonate
-	float y;
-	//Node type
-	Node_Type type;
-	//Node successors
-	int *successors;
-	//Next node
-	struct Node *next;
-	//Next node
-	struct Node *prev;
-}Node;
+	float x; // x Position
+	float y; // y Position
+	Node_Type type; // Node type
+	int *successors; // Node successors
+	struct Node *next; // Next node
+	struct Node *prev; // Previous node
+} Node;
 
-//List of node struct
+
+// Node list struct
 typedef struct List_Node {
-	//lenght of list
-	size_t length;
-	Node *head; //pointeur to first element
-	Node *tail; //pointeur to last element
-}List_Node;
+	size_t length; // list length
+	Node *head; // Pointer to first element
+	Node *tail; // Pointer to last element
+} List_Node;
 
-//Init node's list
+
+// Create new list node
 List_Node* new_List_Node();
-//Create node
+// Create new node
 int create_node(Node_Type type, float x, float y, int *successors, List_Node* l_node);
-//int add_node(List_Node*, Node_Type type, float x, float y, int *successors);
-//Add node to list
+// Add node to list
 void add_node_list(Node* n, List_Node* list_node);
-//Delete one node
+// Delete one node
 List_Node* remove_node(List_Node* current_node, Node* current);
-//Delete list of node
+// Delete list of node
 void free_all_node (List_Node* list_node);
-//Remove node by position
+// Remove node by position
 List_Node* free_node_by_position(List_Node* list_node, Node* current);
 
 #endif
--- src/node.c
+++ src/node.c
@@ -1,10 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "../include/node.h"
 
-
-/* Nvlle liste noeud */
 List_Node* new_List_Node() {
 	List_Node* new_lnode = malloc(sizeof(List_Node));
 	if (new_lnode != NULL) {
@@ -15,60 +10,8 @@ List_Node* new_List_Node() {
 	return new_lnode;
 }
 
-/* Nv noeud dans liste */
-/*
-int add_node(List_Node* list_node, Node_Type type, float x, float y, int *successors) {
-
-	// On vérifie si notre liste a été allouée
-	if (list_node != NULL) {
-		//Création d'un nouveau noeud
-		Node* new_node = malloc(sizeof(Node));
-		int *successors = malloc(sizeof(int*));
-
-		// On vérifie si le malloc n'a pas échoué
-		if (new_node != NULL) {
-			
-			new_node->type = type;
-			new_node->x = x; 
-			new_node->y = y; 
-			printf("New node y position %f\n", new_node->y);
-			new_node->successors = successors;
-			// Rajoute à la fin : dernier élement de la liste 
-			new_node->next = NULL; 
-
-			add_node_list(new_node, list_node);
-
-			// Cas où notre liste est vide (pointeur vers fin de liste à  NULL)
-			if (list_node->head == NULL) {
-				list_node->head = new_node; // Pointe la tête de la liste sur le nouveau noeud
-			} else { // Cas où des éléments sont déjà présents dans la  liste
-				new_node->next = list_node->head;
-				list_node->head = new_node;  // Relie le dernier de la liste au nouveau noeud
-			}
-
-			// Pointe la fin de la liste sur le nouveau noeud
-			
-
-			// On augmente de 1 la taille de la liste
-			list_node->length++; 
-		}
-		else {
-			printf("Not enough memory\n");
-			return 0;
-		}
-	}
-	else {
-		printf("Node list doesn't exist\n");
-		return 0;
-	}
-
-	printf("Node added\n");
-	return 1; 
-}*/
-
 int create_node(Node_Type type, float x, float y, int *successors, List_Node* l_node){
 	Node* n = malloc(sizeof(Node));
-	//int *n_successors = malloc(sizeof(int*));
 
 	if(n != NULL) {
 		n->type = type; //type
@@ -77,9 +20,9 @@ int create_node(Node_Type type, float x, float y, int *successors, List_Node* l_
 		n->successors = successors;
 		n->next = NULL;
 		printf("New node y position %f\n", n->y);
-
 		add_node_list(n, l_node);
 		return 1;
+
 	} else {
 		printf("%s\n", "Not enough memory for node");
 		return 0;
@@ -89,89 +32,68 @@ int create_node(Node_Type type, float x, float y, int *successors, List_Node* l_
 void add_node_list(Node* n, List_Node* list_node){
 	if (list_node != NULL) {
 		if (list_node->tail == NULL) { 
-				// Pointe la tête de la liste sur le nouveau monstre
+				// List head on new node
 				list_node->head = n;
 			}
-			// Cas où des éléments sont déjà présents dans la  liste
+			// If elements are already into list
 			else {
 				n->prev = list_node->tail; 
 				list_node->tail->next = n;
 			}
 			
-			// Pointe la fin de la liste sur le nouveau monstre
+			// List last on new node
 			list_node->tail = n;
+			list_node->length++;
 
-			// On augmente de 1 la taille de la liste
-			list_node->length++; 
-		}
-	else {
-		printf("%s\n", "Fail to add to list node");
+		} else {
+			printf("Fail to add to list node\n");
 	}
 }
 
-
 List_Node* remove_node(List_Node* current_node, Node* current) {
-
-	// On vérifie si notre liste a été allouée
+	// Check if list allocated
 	if (current_node != NULL) {
-
 		if(current != NULL) {
-
-			//Pointe la tête de la liste vers le noeud suivante
+			// List head on next node
 			current_node->head = current->next;
-/*
-			if(current_node->head == NULL) 
-				current_node->tail = NULL;
-			*/
 			free(current);
-			//Décrémente de un la taille de la liste
 			current_node->length--;
 
-		}
-		else
-			printf("%s\n","node doesn't existrs");
-	}
-	else 
-		printf("%s\n","couldn't find list");
+		} else
+			printf("Inexistant node\n");
+	} else 
+		printf("Couldn't find list\n");
 
-	// on retourne notre nouvelle liste
+	// Return new list
 	return current_node; 
 }
 
 void free_all_node(List_Node* list_node) {
-	//Si la liste n'est pas vide
+	// If list not empty
 	if (list_node->length != 0) {
-
-		//Tant que la liste n'est pas vide
+		//	While list not NULL
 		while (list_node->head != NULL) {
 			list_node = remove_node(list_node, list_node->head);
 		}
-		
 	}
+
 	free(list_node);
 }
 
 List_Node* free_node_by_position(List_Node* list_node, Node* current) {
 	if (list_node != NULL) {
-
 		if(current != NULL) {
-
-			//Pointe la tête de la liste vers le noeud suivante
+			// List head on new node
 			list_node->head = current->next;
-/*
-			if(list_node->head == NULL) 
-				list_node->tail = NULL;
-			*/
-			//Libère espace mémoire : supprime la tour
+			// Free memory : delete node
 			free(current);
-			//Décrémente de un la taille de la liste
 			list_node->length--;
-
 		}
 		else
-			fprintf(stderr, "Ce noeud n'existe pas");
+			printf("Inexistant node\n");
 	}
 	else 
-		fprintf(stderr, "Cette liste de noeuds n'existe pas");
+		printf("Inexistant node list\n");
+	
 	return list_node; 
 }
